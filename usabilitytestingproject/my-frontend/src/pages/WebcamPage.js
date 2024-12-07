98df417e4773160f2b36e99e7b3dda73bc9fec42
Min Thuta Naing <6531503157@lamduan.mfu.ac.th>
2024-12-07 12:50:50 +0700

    adjusting the size of emotion chart

--- usabilitytestingproject/my-frontend/src/pages/WebcamPage.js
+++ usabilitytestingproject/my-frontend/src/pages/WebcamPage.js
@@ -282,11 +282,12 @@ function WebcamPage(){
                 padding: "20px", // Increased padding for extra space
                 borderRadius: "8px",
                 border: "1px solid #ccc", // Optional border for clarity
-                width: "90%", // Adjust width if needed
-                height: "400px", // Set a fixed height for better Y-axis visibility
+                width: "1000px", // Adjust width if needed
+                height: "600px", // Set a fixed height for better Y-axis visibility
             }}>
                 <h3>Emotion History Chart</h3>
-                <Line data={chartData} options={chartOptions} style={{ width: "100%", height: "300px" }}/>
+                <Line data={chartData} options={chartOptions} style={{ width: "100%", height: "500px" }} />
+                
             </div>
         )}
         </div>
